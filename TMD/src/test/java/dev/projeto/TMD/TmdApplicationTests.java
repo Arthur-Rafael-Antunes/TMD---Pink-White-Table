4c06ef00446515eecde6ac609e7bb04293bff7cc
Arthur-Rafael-Antunes <130070188+Arthur-Rafael-Antunes@users.noreply.github.com>
2024-07-23 15:01:13 -0300

    Add files via upload

---
+++ TMD/src/test/java/dev/projeto/TMD/TmdApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.projeto.TMD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
