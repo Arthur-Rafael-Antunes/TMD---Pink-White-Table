9d49626963f2536c51df105b020a66492ac2d84c
Arthur-Rafael-Antunes <130070188+Arthur-Rafael-Antunes@users.noreply.github.com>
2024-07-28 11:57:43 -0300

    Add files via upload
    Update para o "TMD"

--- TMD/src/main/java/dev/projeto/TMD/Controle.java
+++ TMD/src/main/java/dev/projeto/TMD/Controle.java
@@ -2,6 +2,8 @@ package dev.projeto.TMD;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class Controle {
 
@@ -10,9 +12,14 @@ public class Controle {
         model.addAttribute("message", "Está funcionando!");
         return "index"; // Thymeleaf
     }
+
     @GetMapping("/calculadora")
     public String calculadora(Model model) {
         return "calculadora"; // Thymeleaf
     }
 
+    @GetMapping("/pesquisa")
+    public String pesquisa(Model model){
+        return "pesquisa";
+    }
 }
